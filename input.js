207ec707383582077581e9d95923115657a7d37c
sankettandulwadkar <sankettandulwadkar@gmail.com>
2014-11-06 18:19:50 -0500

    Adding biography, resume and contact information

---
+++ input.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  	$("#homeTab").attr("class","active");
+
+
+  	$("#tab1").append("<h1 align='center'>Bio<h1>");
+    $("#tab1").append('<p class="centeredImage"><img src="sanket.jpg" alt="Profile Picture" style="width:250px;height:300px" align="middle"></p>');
+    $("#tab1").append('<p id="information" align="center"><br><br>I am Sanket Tandulwadkar. I am a graduate student at University of Massachusetts Lowell. I am currently in my second year. I am interning as a Software Engineering intern at Cengage Learning, Boston. I am taking Internet and Web Systems I, Advanced Database Systems, Data Mining at the University. I moved to the United States August, 2013 to pursue MS in Computer Science. It has been an exciting journey here, learning new culture and a different way of life. My interests include music, cooking, eating, soccer, hiking.</p>');
+     
+
+    $("#tab3").append('<p align="center"><strong>Phone</strong> : (978) 427-3524<br> </p>');
+    $("#tab3").append('<p align="center"><strong>Email</strong> : sanket_tandulwadkar@student.uml.edu<br> </p>');
+    $("#tab3").append('<p align="center"><strong>Address</strong> : 42 Gershom Avenue, Apartment 1, Lowell, MA 01854<br> </p>');
+    $("#tab3").append('<p align="center"><strong>GitHub</strong> : <a target="_blank" href="https://www.github.com/sankettandulwadkar">sankettandulwadkar</a><br> </p>');
+    $("#tab3").append('<p align="center"><strong>LinkedIn</strong> : <a target="_blank" href="https://www.linkedin.com/in/sankettandulwadkar/">Sanket Tandulwadkar</a><br> </p>');
+    $("#tab3").append('<p align="center"><strong>Facebook</strong> : <a target="_blank" href="https://www.facebook.com/sankettandulwadkar">Sanket Tandulwadkar</a> </p>');
+    $("#tab3").append('<p align="center"><strong>Personal Website</strong> : <a target="_blank" href="https://www.sankettandulwadkar.com">Sanket Tandulwadkar</a> </p>');
+    
+
+});
